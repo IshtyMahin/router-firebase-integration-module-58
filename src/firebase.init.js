42c9d6e003f174a7c61347c313dc096520bf0cd1
Ishtiaq uddin <ishty119@gmail.com>
2022-04-11 12:59:57 +0600

    react router install

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBfWMnR1SyiWEY_Mwz5zrtytbhfhKx1BCg",
+  authDomain: "router-firebase-integrat-eca3d.firebaseapp.com",
+  projectId: "router-firebase-integrat-eca3d",
+  storageBucket: "router-firebase-integrat-eca3d.appspot.com",
+  messagingSenderId: "645933661820",
+  appId: "1:645933661820:web:b1996fe240e3d692aa93f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
