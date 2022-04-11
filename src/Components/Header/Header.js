ff600b65b3e16a61fcaa6ff7aad8c5f3ce2b7dcb
Ishtiaq uddin <ishty119@gmail.com>
2022-04-11 14:39:32 +0600

    login and signout

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 import "./Header.css"
 const Header = () => {
+    const {user} =useFirebase();
     return (
         <div className='header'>
             <nav>
@@ -9,7 +11,13 @@ const Header = () => {
             <Link to="/products"> Products</Link>
             <Link to="/orders"> Orders</Link>
             <Link to="/register"> Register</Link>
-            <Link to="/login">LogIn</Link>
+            {   
+                user.uid 
+                ? 
+                <button>SignOut</button>
+                :
+                <Link to="/login">LogIn</Link>
+                }
             </nav>
         </div>
     );
--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const LogIn = () => {
+    const {signInWithGoogle} = useFirebase()
+
     return (
         <div>
             <h3>Please Login</h3>
+            <div style={{margin: '20px'}}>
+            <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+           
             <form>
                 
                 <input type="email"  placeholder='Your email'/>
